a269eeefe49357a7d1372770e235f0bb9adce98d
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2023-12-22 11:32:33 +0530

    till form-validation

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+import { formValues } from "./types/types";
+
+let renderCount = 0;
+
+const YoutubeForm = () => {
+  const form = useForm<formValues>({
+    defaultValues: {
+      username: "Utkarsh Sharma",
+      email: "",
+      channel: "",
+      social: {
+        facebook: "",
+        twitter: "",
+      },
+      phoneNumbers: [" ", " "],
+    },
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: formValues) => {
+    console.log("form data", data);
+  };
+  return (
+    <div>
+      <h1>Dummy Form({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "username is required",
+              },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
+          {/* Utilizes the register function to associate this input with the form state managed by react-hook-form */}
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              validate: {
+                noAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "megayt750@gmail.com" ||
+                    "Enter a valid address"
+                  );
+                },
+                noBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: {
+                value: true,
+                message: "channel is required",
+              },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register("social.twitter")} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="facebook" {...register("social.facebook")} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="primary-phone">Primary Phone Number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0")}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="secondary-phone">Secondary Phone Number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1")}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default YoutubeForm;
---
+++ src/components/types/types.ts
@@ -0,0 +1,10 @@
+export type formValues = {
+    username: string;
+    email: string;
+    channel: string;
+    social: {
+        facebook : string;
+        twitter : string;
+    }
+    phoneNumbers: string[]
+  };
